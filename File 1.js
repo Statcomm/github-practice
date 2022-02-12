a0e9265815abd1b349607b149dd03a3bec8a574c
Statcomm <63654894+Statcomm@users.noreply.github.com>
2022-02-12 13:03:08 +0300

    jo

--- File 1.js
+++ File 1.js
@@ -1,6 +1,5 @@
 console.log("Bader messed up and updated the main");
 console.log("Hamed accidentally worked on main");
-console.log("Bader messed up and updated the main");
 
 console.log("hamed sync test");
 console.log("Ahmed Messed the update");
